e59742a5e1453956ff3f23693357ccbc37d8fee7
Daria Solomkina <127635866+dasha-solomkina@users.noreply.github.com>
2026-09-04 10:20:45 -0300

    solve bubble sort

--- sort/bubble.js
+++ sort/bubble.js
@@ -1,4 +1,25 @@
 export function bubbleSort(arr) {
+    for (let j = 0; j < arr.length; j++ ) {
+        let wasSubMade = false // to improve Omega time notation
+        for (let i = 0; i < arr.length - 1 - j; i++ ) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                wasSubMade = true // to improve Omega time notation
+            }
+            
+        }
+        if (!wasSubMade) return arr // to improve Omega time notation
+    }
 
-    return [...arr]
+    return arr
 }
+
+
+// Time:
+// Big O (N^2)
+// Omega (N^2) -> after the improvement O(N)
+
+// Space: 
+// Theta O(1)
