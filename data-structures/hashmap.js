d0ff75ed1b3e6267db1d4097ff33a1fde1a225e1
Daria Solomkina <127635866+dasha-solomkina@users.noreply.github.com>
2026-09-21 13:40:52 -0300

    finish hashmap

--- data-structures/hashmap.js
+++ data-structures/hashmap.js
@@ -6,20 +6,18 @@ const createHashMap = () => {
 
     const loadFactor = 0.75
     let capacity = 16
+    let size = 0
 
-    const table = new Array(capacity).fill(null);
+    let table = new Array(capacity).fill(null);
 
     return {
         hash(key) {
             let hashCode = 0
             const primaryNumber = 31
 
-
             for (let i = 0; i < key.length; i++) {
-                hashCode = primaryNumber * hashCode + key.charCodeAt(i)
+                hashCode = (primaryNumber * hashCode + key.charCodeAt(i)) % capacity
             }
-
-            hashCode = hashCode % capacity
 
             return hashCode
         },
@@ -34,19 +32,18 @@ const createHashMap = () => {
             if (table[hashCode] === null) {
                 const newNode = createNode(key, value)
                 table[hashCode] = newNode
+                size++
             } else {
-                let currentNode = table[hashCode]
 
-                if (currentNode.key === key) {
-                    currentNode.value = value
-                    return
-                }
-
+                const dummy = createNode(null, null, table[hashCode])
+                
+                let currentNode = dummy
                 while (currentNode.nextNode != null) {
                     const nextNode = currentNode.nextNode
 
                     if (nextNode.key === key) {
                         nextNode.value = value
+                        table[hashCode] = dummy.nextNode
                         return
                     }
 
@@ -55,6 +52,22 @@ const createHashMap = () => {
 
                 const newNode = createNode(key, value) 
                 currentNode.nextNode = newNode
+
+                table[hashCode] = dummy.nextNode
+                size++
+            }
+
+            const currentLoadFactor = size / capacity
+
+            if (currentLoadFactor > loadFactor) {
+                capacity = capacity * 2
+                const oldValues = this.entries()
+                table = new Array(capacity).fill(null);
+                size = 0
+
+                for (const [k, v] of oldValues) {
+                    this.set(k, v)
+                }
             }
         },
 
@@ -66,7 +79,7 @@ const createHashMap = () => {
                 throw new Error("Trying to access index out of bounds");
             }
 
-            if (table[hashCode] === null) return undefined
+            if (table[hashCode] === null) return null
 
             let currentNode = table[hashCode]
 
@@ -77,11 +90,14 @@ const createHashMap = () => {
                     currentNode = currentNode.nextNode
                 }
             }
-            return undefined
+            return null
         },
 
         // takes a key as an argument and returns a boolean based on whether or not the key is in the hash map.
         has(key) {
+
+            // Could have written: return this.get(key) !== null;
+
             const hashCode = this.hash(key)
             if (hashCode < 0 || hashCode >= capacity) {
                 throw new Error("Trying to access index out of bounds");
@@ -108,28 +124,24 @@ const createHashMap = () => {
             }
 
             if (table[hashCode] === null) return false
-            
-            let currentNode = table[hashCode]
 
-            if (currentNode.key === key) {
-                const nextNodes = currentNode.nextNode
-                table[hashCode] = nextNodes
-                return true
-            }
+            const dummy = createNode(null, null, table[hashCode])
+            
+            let currentNode = dummy
 
             while (currentNode.nextNode != null) {
                 const childNode = currentNode.nextNode
 
                 if (childNode.key === key) {
-                    const nextNodes =  childNode.nextNode
+                    const nextNodes = childNode.nextNode
                     currentNode.nextNode = nextNodes
+                    table[hashCode] = dummy.nextNode
                     return true
                 }
                 currentNode = currentNode.nextNode
             }
 
             return false
-
         },
 
         // returns the number of stored keys in the hash map.
@@ -151,7 +163,7 @@ const createHashMap = () => {
 
         // removes all entries in the hash map.
         clear() {
-            return table.fill(null)
+            table.fill(null)
         },
 
         // returns an array containing all the keys (not values) inside the hash map.
@@ -209,6 +221,7 @@ const createHashMap = () => {
 
 const test = createHashMap()
 
+
 test.set('apple', 'red')
 test.set('banana', 'yellow')
 test.set('carrot', 'orange')
@@ -220,12 +233,11 @@ test.set('hat', 'black')
 test.set('ice cream', 'white')
 test.set('jacket', 'blue')
 test.set('kite', 'pink')
-test.set('lion', 'golden')
+test.set('lion', 'golden') // 12
+test.set('moon', 'silver') // 13
 
 const output = test.length()
 console.log({output})
-
-
 
 
 
